eecc8c6a3b99d8ceea368d1b5353897cd6d1cd7f
Ilya Beda <ir4y.ix@gmail.com>
2013-03-29 23:54:09 +0800

    Add defargs module
    add fuction getfull-args for arg list parding

---
+++ src/learn_macros/defargs.clj
@@ -0,0 +1,35 @@
+(ns learn-macros.defargs)
+
+(comment
+  (defargs sum [a 1 b 2]
+      (+ a b))
+
+  (sum 0 0) => 0
+  (sum 0) => 2
+  (sum) => 3
+
+  (defargs sum-and-print [a 1 b 2]
+      (println "a =" a "b =" b)
+      (+ a b))
+
+  (sum-and-print 2) => 4)
+
+(def args [:a 1 :b 2])
+(first args)
+(first (next args))
+(next (next args))
+
+(defn getfull-args [[key value] & args]
+  (if (nil? args) 
+    [key]
+    (concat [key] (apply getfull-args args))))
+
+(defmacro defargs [name args body]
+  (let [full-args (apply getfull-args (partition 2 args))]
+  `(do (defn ~name ~full-args ~body))))
+
+(macroexpand-1 '(defargs sum [a 1 b 2](+ a b)))
+
+(defargs sum [a b] (+ a b))
+(sum 1 2)
+
