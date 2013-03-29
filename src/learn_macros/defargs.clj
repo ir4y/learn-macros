348ac205b3374c428ed7f0c92485f85f121d1470
Ilya Beda <ir4y.ix@gmail.com>
2013-03-30 01:00:16 +0800

    Add get-compositions function

--- src/learn_macros/defargs.clj
+++ src/learn_macros/defargs.clj
@@ -24,18 +24,25 @@
     [value]
     (concat [value] (apply getfull-args args))))
 
-(defn get-compositions [full-args body]
-     `(~(vec full-args) ~body)
-  )
+(defn get-compositions [args default-args default-values body]
+  (if (nil? default-args)
+     `(~(vec args) ~body)
+     `(~(vec args) (let ~(vec (interleave default-args default-values)) ~body))))
 
 (defmacro defargs [name args body]
   (let [full-args (apply getfull-args (partition 2 args))
-        full-values (apply getfull-values (partition 2 args))]
+        full-values (apply getfull-values (partition 2 args))
+        a (symbol "a") 
+        b (symbol "b")]
     `(defn ~name 
-       ~(get-compositions full-args body))))
+        ~(get-compositions [a b] nil nil body)
+        ~(get-compositions [a] [b] [1] body)
+        ~(get-compositions [] [a b] [2 1] body))))
 
 (macroexpand-1 '(defargs sum [a 1 b 2](+ a b)))
 
 (defargs sum [a 1 b 2] (+ a b))
 (sum 1 2)
+(sum 1)
+(sum)
 
