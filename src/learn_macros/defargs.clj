02508820c342d553012934473d049174d1150e9d
Ilya Beda <ir4y.ix@gmail.com>
2013-03-30 22:30:54 +0800

    Varaible name refactor

--- src/learn_macros/defargs.clj
+++ src/learn_macros/defargs.clj
@@ -1,19 +1,19 @@
 (ns learn-macros.defargs)
 
-(defn get-compositions [args default-args body]
-  (if (= default-args [])
+(defn get-compositions [args default-values body]
+  (if (= default-values [])
      `(~(vec args) ~@body)
-     `(~(vec args) (let ~(vec (flatten default-args)) ~@body))))
+     `(~(vec args) (let ~(vec (flatten default-values)) ~@body))))
 
-(defn get-function-body [args default-args use-args body]
+(defn get-function-body [args default-values use-args body]
   (if (= args '())
     [(get-compositions args use-args body)]
     (concat
       [(get-compositions args use-args body)]
       (get-function-body 
         (drop-last args)
-        (drop-last 2 default-args)
-        (concat use-args [(take-last 2 default-args)])
+        (drop-last 2 default-values)
+        (concat use-args [(take-last 2 default-values)])
         body))))
 
 (defmacro defargs [name args & body]
